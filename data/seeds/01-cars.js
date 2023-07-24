149b8484676ba6d58163a3952784ccf1a80a618b
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-07-24 16:54:17 -0400

    dealers.db created

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -9,14 +9,14 @@ const cars = [
         transmission: 'manual'
     }, 
     {
-        vin: "1111111111111",
+        vin: "1111111111112",
         make: "toyota",
         model: "corolla",
         mileage: 115000,
         title: 'salvage'
     },
     {
-        vin: "1111111111111",
+        vin: "1111111111113",
         make: "ford",
         model: "focus",
         mileage: 15000
