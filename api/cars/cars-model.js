fe126ab1a253920b3a19b200aab32aa41d7f3cf4
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-07-24 21:25:36 -0400

    get endpoint to get all cars plus getAll function working

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,20 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
-}
+  return db('cars');
+};
 
 const getById = () => {
   // DO YOUR MAGIC
-}
+};
 
 const create = () => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,10 +1,16 @@
 // DO YOUR MAGIC
 const express = require('express');
+const Car = require('./cars-model');
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    res.json('getting all cars');
+    try {
+        const cars = await Car.getAll();
+        res.json(cars);
+    } catch (err) {
+        next(err);
+    }
 });
 
 router.get('/:id', async (req, res, next) => {
