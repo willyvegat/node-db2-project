6734585de398241b8e864b638972ddd6f5884867
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-07-26 15:11:53 -0400

    error 400 for vin make model and mileage works

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -14,7 +14,33 @@ const checkCarId = async (req, res, next) => {
 }};
 
 const checkCarPayload = (req, res, next) => {
-  next();
+  // const error = { status: 400 }; 
+  //   || !make || !model || !mileage) {
+    //   next({ status: 400, message: ` is missing`});
+    // }
+
+  const { vin, make, model, mileage } = req.body;
+  if (!vin) return next(
+    { 
+      status: 400, 
+      message: 'vin is missing' 
+    }); 
+  if (!make) return next(
+    { 
+      status: 400, 
+      message: 'make is missing' 
+    }); 
+  if (!model) return next(
+    { 
+      status: 400, 
+      message: 'model is missing' 
+    }); 
+  if (!mileage) return next(
+    { 
+      status: 400, 
+      message: 'mileage is missing' 
+    });
+    next();
 };
 
 const checkVinNumberValid = (req, res, next) => {
